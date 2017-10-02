3af7f47b28761cb17815d21dee8cf6c8a807571c
justbinaryy <32437710+justbinaryy@users.noreply.github.com>
2017-10-02 08:25:05 +0200

    Add files via upload

---
+++ new.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-1-1 --off --output VIRTUAL1 --off --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-3 --off --output HDMI2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --off --output DP-1-4 --off --output DP-1-3 --off --output DP-1-2 --off
